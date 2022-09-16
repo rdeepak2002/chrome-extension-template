7c01f3ef03f2afad6df93e6e025850257451ad8c
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-09-16 15:11:50 -0400

    Fix lint check GitHub action

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
 // show pages/help.html when the extension is first installed
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.tabs.create({  url: chrome.runtime.getURL('/pages/help.html') });
+  chrome.tabs.create({ url: chrome.runtime.getURL('/pages/help.html') });
 });
